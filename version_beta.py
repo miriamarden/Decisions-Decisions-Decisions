ed1e91842113583df98db38c4713602c18b0ff4a
revann <79774439+revann@users.noreply.github.com>
2021-04-16 10:21:24 -0400

    adding text on pygame screen

--- version_beta.py
+++ version_beta.py
@@ -1,11 +1,13 @@
 # testing out basic pygame stuff
 import pygame
 import os
+
+pygame.init()
 width, height = 1200, 900
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Decisions, Decisions, Decisions')
 
-#initializing variables
+# initializing variables
 white = (255, 255, 255)
 blue_green = (155, 250, 255)
 magenta = (255, 155, 255)
@@ -13,23 +15,32 @@ red = (255, 75, 75)
 VEL = 5
 FPS = 60
 
-#images
-#from stack overflow
+font = pygame.font.SysFont('georgia.ttf', 24)
+test_font = font.render('Hello', True, white)
+
+# images
+# from stack overflow
 # rect = rect.move((x,y))
 # screen.blit(img_name, rect)
-#define screen size
-main_green = pygame.image.load(os.path.join('EFFECTS.jpg'))
+# define screen size
+main_green = pygame.image.load(os.path.join('main_green.jpg'))
 main_green = pygame.transform.scale(main_green, (width, height))
 bear_img = pygame.image.load(os.path.join('bear_red.png'))
 bear = pygame.transform.scale(bear_img, (184, 178))
-#bear_rect = bear.get_rect()
-#SPACE = pygame.transform.scale(pygame.image.load(
-#os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
-#pygame.display.flip()
+# bear_rect = bear.get_rect()
+# SPACE = pygame.transform.scale(pygame.image.load(
+# os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
+# pygame.display.flip()
+
+# font stuff options: 'timesnewroman', 'georgia', 'helvetica'
+# 1. create font object
+# 2. render text into image with given color
+# 3. blit image to screen
 
 
-def draw_window(magenta_rect, bear):
-    win.blit(main_green, (0, 0))
+def draw_window(magenta_rect, bear, test_font):
+    win.fill(red)
+    win.blit(test_font, (400, 300))
     win.blit(bear, (magenta_rect.x, magenta_rect.y))
     pygame.display.flip()
 
@@ -46,6 +57,7 @@ def magenta_movement(keys_pressed, magenta_rect):
 
 
 def main():
+    #background = win.fill(red)
     magenta_rect = pygame.Rect(0, 0, 50, 50)
     print(magenta_rect.x)
     clock = pygame.time.Clock()
@@ -57,9 +69,8 @@ def main():
                 run = False
                 pygame.quit()
         keys_pressed = pygame.key.get_pressed()
-        #pygame.draw.rect(win, magenta, magenta_rect)
         magenta_movement(keys_pressed, magenta_rect)
-        draw_window(magenta_rect, bear)
+        draw_window(magenta_rect, bear, test_font)
         pygame.display.flip()
     main()
 
