b56c9393616a070c26778ab178bcb90d5baa919e
revann <79774439+revann@users.noreply.github.com>
2021-04-17 16:16:53 -0400

    updated response display and added ptext.draw text as variable, other minor changes too

--- version_beta.py
+++ version_beta.py
@@ -22,10 +22,6 @@ test_font = font.render('Hello', True, white)
 welcome = font.render('Welcome to the Brown experience! Today Ill be your guide.', True, white)
 
 # images
-# from stack overflow
-# rect = rect.move((x,y))
-# screen.blit(img_name, rect)
-# define screen size
 main_green = pygame.image.load(os.path.join('main_green.jpg'))
 main_green = pygame.transform.scale(main_green, (width, height))
 bear_img = pygame.image.load(os.path.join('bear_red.png'))
@@ -33,9 +29,6 @@ bear = pygame.transform.scale(bear_img, (184, 178))
 a_let = pygame.transform.scale(pygame.image.load(os.path.join('Option A.png')), (80, 80))
 b_let = pygame.transform.scale(pygame.image.load(os.path.join('Option B.png')), (80, 80))
 # bear_rect = bear.get_rect()
-# SPACE = pygame.transform.scale(pygame.image.load(
-# os.path.join('Assets', 'space.png')), (WIDTH, HEIGHT))
-# pygame.display.flip()
 
 # font stuff options: 'timesnewroman', 'georgia', 'helvetica'
 # 1. create font object
@@ -43,13 +36,15 @@ b_let = pygame.transform.scale(pygame.image.load(os.path.join('Option B.png')), 
 # 3. blit image to screen
 
 
-def draw_window(bear_rect, a_position, b_position, a_let, b_let, bear):
+def draw_window(bear_rect, a_position, b_position, a_let, b_let, bear, string_bear, string_a, string_b):
     win.fill(red)
     win.blit(bear, (bear_rect.x, bear_rect.y))
     win.blit(a_let, (a_position.x, a_position.y))
     win.blit(b_let, (b_position.x, b_position.y))
     text_location = [bear_rect.x - 400, bear_rect.y - 100]
-    ptext.draw('Honorable choice. You venture into the Brown bookstore with a flexible budget of $60 looking for some merch. At the front of the bookstore is a grand display of Browns new line of hoodies. Each hoodie costs $100. You search in the back of the store for other hoodie options and find a cute hoodie from last year for $80. Do you', (text_location[0], text_location[1]), align="center",  width=400)
+    ptext.draw(string_bear, (text_location[0], text_location[1]), align="center",  width=400)
+    ptext.draw(string_a, (a_position.x, a_position.y - 80))
+    ptext.draw(string_b, (b_position.x, b_position.y - 80))
     pygame.display.flip()
 
 
@@ -64,18 +59,18 @@ def bear_movement(keys_pressed, bear_rect):
         bear_rect.y += VEL
 
 
-# testing loop with questions ...
-# welcome = font.render('Welcome to the Brown experience! Let"s get started!.', True, white)
-
-
 def main():
-    # background = win.fill(red)
     bear_rect = pygame.Rect(1000, 200, 184, 178)
-    a_position = pygame.Rect((width * 0.25)-40, height * 0.75, 80, 80)
-    b_position = pygame.Rect((width * 0.75) -40, height * 0.75, 80, 80)
-    print(bear_rect.x)
+    a_position = pygame.Rect((width * 0.33)-40, height * 0.75, 80, 80)
+    b_position = pygame.Rect((width * 0.66) - 40, height * 0.75, 80, 80)
+    string_bear = 'welcome to the game!'  # no line breaks needed
+    string_a = 'testing again again'  # line breaks needed
+    string_b = 'answer 2 testing testing'  # line breaks needed
     clock = pygame.time.Clock()
     run = True
+    intro = True
+    main_game = True
+    results = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -83,9 +78,20 @@ def main():
                 run = False
                 pygame.quit()
         # start game text
+        # use embedded while loops for intro and game sections
+        while intro:
+            string_bear = 'Welcome to the game, ill be your guide today! Were going to go on an adventure that you choose. To pick option A blah blah blah'
+            # greetings and instructions part
+            # wait function to display text or space bar to continue?
+            intro = False
+        while main_game:
+            #questions loop
+        while results:
+            #DISPLAY RESULTS
+
         keys_pressed = pygame.key.get_pressed()
         bear_movement(keys_pressed, bear_rect)
-        draw_window(bear_rect, a_position, b_position, a_let, b_let, bear)
+        draw_window(bear_rect, a_position, b_position, a_let, b_let, bear, string_bear, string_a, string_b)
         pygame.display.flip()
     main()
 
